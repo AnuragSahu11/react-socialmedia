169653406683fc524c709121c4b142c0aecba17d
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-05-07 18:18:36 +0530

    [feat]primary routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import {
+  Feed,
+  Footer,
+  LandingPage,
+  LoginPage,
+  Navbar,
+  ProfilePage,
+  Sidebar,
+  SignUpPage,
+} from "./components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/user/:userId" element={<ProfilePage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
